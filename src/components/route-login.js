4059bd4e5bfbbb499fc996a22fe9d5dcf0f35d16
Kapil Dev <talktokapildev@gmail.com>
2020-03-28 10:01:10 +0000

    authentication

--- src/components/route-login.js
+++ src/components/route-login.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useIdentityContext } from 'react-netlify-identity';
+import { navigate } from 'gatsby';
 
-const RouteLogin = () => {
+const RouteLogin = ({ showModal }) => {
+  const identity = useIdentityContext();
+
+  if (identity && identity.isLoggedIn) {
+    navigate('/dashboard/secret', { replace: true });
+  }
+
   return (
     <div>
       <h1>Login or Sign up</h1>
-      <button>Log In</button>
+      <button onClick={showModal}>Log In</button>
     </div>
   );
 };
